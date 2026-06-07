0c13f64e39f8555c690a952e2a1ba0c147516143
MarianoBordon8 <bordon.marianooscar@gmail.com>
2026-06-07 17:34:54 -0300

    Sistema de posicionamiento automático 1x2 completado

---
+++ Assets/Scripts/ItemArma.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ItemArma : MonoBehaviour
+{
+    [Header("Dimensiones en la cuadricula")]
+    public int ancho = 2;
+    public int alto = 1;
+
+    [HideInInspector] public int filaActual;
+    [HideInInspector] public int columnaActual;
+}
